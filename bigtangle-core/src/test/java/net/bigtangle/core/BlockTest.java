07c1d9d241090244ffd06722aeeddd56cac9684d
j09004 <j0904@localhost.com>
2025-07-27 12:40:43 +0200

    add test address test

--- bigtangle-core/src/test/java/net/bigtangle/core/BlockTest.java
+++ bigtangle-core/src/test/java/net/bigtangle/core/BlockTest.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import jdk.internal.org.jline.utils.Log;
 import net.bigtangle.exception.VerificationException;
 import net.bigtangle.params.MainNetParams;
 import net.bigtangle.params.NetworkParameters;
@@ -102,7 +103,14 @@ public class BlockTest {
         Block reparsed = PARAMS.getDefaultSerializer().makeBlock(header.bitcoinSerialize());
         assertEquals(reparsed, header);
     }
-
+    @Test
+    public void testBitcoinSerialization1() throws Exception {
+    
+      String t = "01000000ae579cc5d5854d46e38495665fefad8b2dc110a083abcf7dae970bed19f05548ae579cc5d5854d46e38495665"
+    +"fefad8b2dc110a083abcf7dae970bed19f05548170dafec26b25ed20a2c87d485de589c57fc1b32e65a37ea970feb15142b5f1a2a34856800000000ae470120000000000000000000000000cb16b4d62bdf6a05a961cf27a47355486891ebb9ee6892f8010000000100000000000000010100000001ae579cc5d5854d46e38495665fefad8b2dc110a083abcf7dae970bed19f055488b404ea4787ac1af3c007dc3462b9875d320ee983690b649d9c564da7a4c38d50000000049483045022100818570e724f91eb5d73b6a195c905fe7d56414cd39fef6aaba20d10f60402256022011f04e032d4bcd111218d07f32195e29f9e3dbb4ef517f91d596e248f84c08c201ffffffff0100000008016345785d8a000001bc232102721b5eb0282e4bc86aab3380e2bba31d935cba386741c15447973432c61bc975ac02030f424001bc1976a91451d65cb4f2e64551c447cd41635dd9214bbaf19d88ac08016345785d7ab9d801bc232102721b5eb0282e4bc86aab3380e2bba31d935cba386741c15447973432c61bc975ac00000000000000000000000000000000420000007b0a2020226b7622203a205b207b0a20202020226b657922203a20226d656d6f222c0a202020202276616c756522203a20227061794c697374220a20207d205d0a7d00000000";
+       Block tb = PARAMS.getDefaultSerializer().makeBlock(    Utils.HEX.decode(t))    ;
+      System.out.printf(  tb.toString());
+    }
     @Test
     public void testBitcoinSerialization() throws Exception {
         // We have to be able to reserialize everything exactly as we found it
