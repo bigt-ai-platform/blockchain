333083a01e6f2987551e13209421a4ad770e40d5
j0904 <j0904@localhost>
2025-10-31 22:15:10 +0100

    fix remote test

--- bigtangle-server/src/test/java/net/bigtangle/server/performance/RemoteTest.java
+++ bigtangle-server/src/test/java/net/bigtangle/server/performance/RemoteTest.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
