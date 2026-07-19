1f80af129e43457f97389d205f573e4748c7b8b0
j0904 <j0904@localhost>
2026-07-19 12:48:30 +0200

    remove ConstantTest

--- bigtangle-core/src/test/java/net/bigtangle/core/ConstantTest.java
+++ bigtangle-core/src/test/java/net/bigtangle/core/ConstantTest.java
@@ -2,6 +2,7 @@ package net.bigtangle.core;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import net.bigtangle.core.BlockType;
