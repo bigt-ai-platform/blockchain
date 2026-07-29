d34186a1db63b0d8f3a63acf3b43483038b6c5b2
j0904 <j0904@localhost>
2026-07-30 00:08:58 +0200

    bench: 2000 payments/client => 2859 tx/s

--- layer0-mcmc/src/test/java/net/bigtangle/mcmc/test/benchmark/BenchmarkRunner.java
+++ layer0-mcmc/src/test/java/net/bigtangle/mcmc/test/benchmark/BenchmarkRunner.java
@@ -41,7 +41,7 @@ public class BenchmarkRunner {
 
     private static final Logger log = LoggerFactory.getLogger(BenchmarkRunner.class);
     private static final int CLIENTS = 30;
-    private static final int PAYMENTS_PER_CLIENT = 500;
+    private static final int PAYMENTS_PER_CLIENT = 2000;
 
     public static void main(String[] args) throws Exception {
         String serverUrl = args.length > 0 ? args[0] : "http://localhost:8088/";
